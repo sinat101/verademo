8f9610e9f80287fa02a6c0715f3e83dcf07a04c8
Sina Taghizadeh <sinat101@users.noreply.github.com>
2021-12-17 14:33:22 -0500

    Update Jenkinsfile_SCA.groovy

--- Jenkinsfile_SCA.groovy
+++ Jenkinsfile_SCA.groovy
@@ -47,8 +47,7 @@ pipeline {
                                 sh '''
                                     export SCAN_DIR="./app"
                                     touch SCA_Results_Build_${BUILD_NUMBER}.txt
-                                    "curl -sSL https://download.sourceclear.com/ci.sh | bash -s -- scan --update-advisor 2>&1 | tee SCA_Results_Build_${BUILD_NUMBER}.txt"
-                                    grep -E 'CVE-2021-45046|CVE-2021-44228' SCA_Results_Build_${BUILD_NUMBER}.txt
+                                    curl -sSL https://download.sourceclear.com/ci.sh | bash -s -- scan --update-advisor
                                 '''
 
                                 // debug, no upload
