5eb6d354c81c24a790905c580f1d6881e8186825
Sina Taghizadeh <sinat101@users.noreply.github.com>
2022-05-10 15:04:42 -0400

    Create Jenkinsfile_Linux.groovy

---
+++ Jenkinsfile_Linux.groovy
@@ -0,0 +1,70 @@
+/*
+ * Normal Jenkinsfile that will build and do Policy and SCA scans
+ */
+
+pipeline {
+    agent any
+
+    environment {
+        VERACODE_APP_NAME = 'VeraDemo'      // App Name in the Veracode Platform
+    }
+
+    // this is optional on Linux, if jenkins does not have access to your locally installed docker
+    //tools {
+        // these match up with 'Manage Jenkins -> Global Tool Config'
+        //'org.jenkinsci.plugins.docker.commons.tools.DockerTool' 'docker-latest' 
+    //}
+
+    options {
+        // only keep the last x build logs and artifacts (for space saving)
+        buildDiscarder(logRotator(numToKeepStr: '20', artifactNumToKeepStr: '20'))
+    }
+
+    stages{
+
+        stage ('build') {
+            steps {
+                withMaven(maven:'maven-3') {
+                    script {
+                            sh 'mvn clean package'
+                    }
+                }
+            }
+        }
+
+        stage ('Veracode scan') {
+            steps {
+                echo 'Veracode scanning'
+                withCredentials([ usernamePassword ( 
+                    credentialsId: 'veracode_login', usernameVariable: 'VERACODE_API_ID', passwordVariable: 'VERACODE_API_KEY') ]) {
+                        // fire-and-forget 
+                        veracode applicationName: "${VERACODE_APP_NAME}", +
+                        scanName: "${BUILD_TAG}-${env.HOST_OS}", +
+                        uploadIncludesPattern: 'target/verademo.war', +
+                        vid: "${VERACODE_API_ID}", +
+                        vkey: "${VERACODE_API_KEY}"
+
+                        // wait for scan to complete (timeout: x)
+                        //veracode applicationName: '${VERACODE_APP_NAME}'', criticality: 'VeryHigh', debug: true, timeout: 20, fileNamePattern: '', pHost: '', pPassword: '', pUser: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: "${BUILD_TAG}", uploadExcludesPattern: '', uploadIncludesPattern: 'target/verademo.war', vid: '${VERACODE_API_ID}', vkey: '${VERACODE_API_KEY}'
+                    }      
+            }
+        }
+
+        stage ('Veracode SCA') {
+            steps {
+                echo 'Veracode SCA'
+                withCredentials([ string(credentialsId: 'SCA_Token', variable: 'SRCCLR_API_TOKEN')]) {
+                    withMaven(maven:'maven-3') {
+                        script {
+                            sh '''
+                                export SCAN_DIR="./app"
+                                curl -sSL https://download.sourceclear.com/ci.sh | bash -s scan --update-advisor
+                            '''
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
