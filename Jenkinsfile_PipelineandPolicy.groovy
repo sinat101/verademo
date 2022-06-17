1b1fb31cbe1a08b8aae06584add5e95563864e85
Sina Taghizadeh <sinat101@users.noreply.github.com>
2022-06-17 11:19:51 -0400

    Create Jenkinsfile_PipelineandPolicy.groovy

---
+++ Jenkinsfile_PipelineandPolicy.groovy
@@ -0,0 +1,67 @@
+/*
+ * Normal Jenkinsfile that will build and do Policy and SCA scans
+ */
+
+pipeline {
+    agent any
+
+    environment {
+        VERACODE_APP_NAME = 'VeraDemo'      // App Name in the Veracode Platform
+    }
+
+    // this is optional on Linux, if jenkins does not have access to your locally installed docker
+    //tools {
+        // these match up with 'Manage Jenkins -> Global Tool Config'
+        //'org.jenkinsci.plugins.docker.commons.tools.DockerTool' 'docker-latest' 
+    //}
+
+    options {
+        // only keep the last x build logs and artifacts (for space saving)
+        buildDiscarder(logRotator(numToKeepStr: '20', artifactNumToKeepStr: '20'))
+    }
+
+    stages{
+
+        stage ('build') {
+            steps {
+                withMaven(maven:'maven-3') {
+                    script {
+                        dir('app') {
+                            sh 'mvn clean package'
+                        }
+                    }
+                }
+            }
+        }
+
+        stage ('Policy Scan') {
+            steps {
+                echo 'Veracode scanning'
+                withCredentials([ usernamePassword ( 
+                    credentialsId: 'veracode_login', usernameVariable: 'VERACODE_API_ID', passwordVariable: 'VERACODE_API_KEY') ]) {
+                        // fire-and-forget 
+                        veracode applicationName: "${VERACODE_APP_NAME}", uploadIncludesPattern: "app/target/verademo.war", scanName: "${BUILD_TAG}-${env.HOST_OS}", vid: "${VERACODE_API_ID}", vkey: "${VERACODE_API_KEY}"
+
+                        // wait for scan to complete (timeout: x)
+                        //veracode applicationName: '${VERACODE_APP_NAME}'', criticality: 'VeryHigh', debug: true, timeout: 20, fileNamePattern: '', pHost: '', pPassword: '', pUser: '', replacementPattern: '', sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '', scanName: "${BUILD_TAG}", uploadExcludesPattern: '', uploadIncludesPattern: 'target/verademo.war', vid: '${VERACODE_API_ID}', vkey: '${VERACODE_API_KEY}'
+                    }      
+            }
+        }
+
+        stage ('Pipeline Scan') {
+            steps {
+                echo 'Pipeline Scan'
+                withCredentials([ usernamePassword (
+                    credentialsId: 'veracode_login', usernameVariable: 'VERACODE_API_ID', passwordVariable: 'VERACODE_API_KEY') ]) {
+                        script {
+                            sh '''
+                                curl -sSO https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip
+      							unzip -o pipeline-scan-LATEST.zip
+      							java -jar pipeline-scan.jar -vid ${VERACODE_API_ID} -vkey ${VERACODE_API_KEY} -f app/target/verademo.war -fs="Very High"
+                            '''
+                            }
+                    }
+                }
+            }
+        }
+    }
